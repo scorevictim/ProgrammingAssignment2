5adf366d84a8c32ca7353f40dd9f1e428efe493a
scorevictim <30682499+scorevictim@users.noreply.github.com>
2021-07-30 17:21:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m<-NULL
+        set<-function(y){
+                x << - y # with << - can be the starting assignment behavior were carried out on the environment hierarchy
+                m<<-NULL
+        }
+        get<-function() x
+        setM<-function(solve) m<<-solve
+        getM<-function() m
+        list(set=set,get=get,setM=setM,getM=getM)
 
 }
 
@@ -11,5 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        m <- x$getM()
+                if(!is.null(m)){
+                message("TO cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setM(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
